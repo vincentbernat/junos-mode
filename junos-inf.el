eb79ecbe42494e20d6dea909f9386b13d1c668e8
Vincent Bernat <vincent@bernat.im>
2015-05-04 17:32:04 +0200

    inf: add an inferior mode for JunOS CLI
    Quite basic. Expects to be able to log with ssh without a prompt.

---
+++ junos-inf.el
@@ -0,0 +1,71 @@
+;;; junos-inf.el --- JunOS inferior mode
+
+;; Copyright (C) 2014  Free Software Foundation, Inc.
+
+;; Author: Vincent Bernat
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
+(require 'comint)
+
+(defvar junos-inf-prompt-regexp "^\\(?:[^@]+@[^@>#]+[>#] \\)"
+  "Prompt for JunOS session.")
+
+(defvar junos-inf-mode-map
+  (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
+    (define-key map "\t" 'completion-at-point)
+    map)
+  "Basic mode map for `run-junos'.")
+
+(defvar junos-inf-font-lock-keywords
+  '((junos-inf-prompt-regexp . 'font-lock-constant-face))
+  "Keyword highlighting specification for `junos-inf-mode'.")
+
+(define-derived-mode junos-inf-mode comint-mode "JunOS"
+  "A major mode for running JunOS CLI.
+
+\\<junos-inf-mode-map>"
+  (setq-local paragraph-start junos-inf-prompt-regexp)
+  (setq-local comint-process-echoes t)
+  (setq-local comint-use-prompt-regexp t)
+  (setq-local comint-prompt-regexp junos-inf-prompt-regexp)
+  (setq-local comint-prompt-read-only t)
+  (setq-local font-lock-defaults
+              '(junos-inf-font-lock-keywords)))
+
+(defun run-junos (host)
+  "Start an a JunOS connection into an inferior process.
+The JunOS connection is made with ssh to the provided
+HOST."
+  (let ((buffer
+         (apply 'make-comint (concat "junos " host) "ssh" nil (list host))))
+    (with-current-buffer buffer
+      (junos-inf-mode))
+    buffer))
+
+(let* ((buffer (run-junos "gv2p-pf-cloud01.exoscale.local"))
+       (p (get-buffer-process buffer)))
+  (comint-send-string p "set cli screen-width 0\n")
+  (comint-send-string p "set cli screen-length 0\n")
+  (comint-send-string p "set cli timestamp\n"))
+
+(provide 'junos-inf)
+;;; junos-inf.el ends here
