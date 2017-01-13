f01748701cc43dd9021db4b51d5081ca043dbb4c
Vincent Bernat <vincent@bernat.im>
2017-01-13 14:22:55 +0100

    babel: also add op and clear
    We could add more, but that should be the most important ones.

--- ob-junos.el
+++ ob-junos.el
@@ -48,7 +48,7 @@
   "Path to junos.py helper.")
 
 (defconst org-babel-junos-cli-commands
-  '("show" "request" "ping")
+  '("show" "request" "ping" "traceroute" "op" "clear")
   "Commands that should be run in a CLI session.")
 
 (defconst org-babel-junos-confirm-minutes
